36aa6862698cd5b57eae206589a8e09aac79726a
Vladislav Topolnyk <byntar33@gmail.com>
2022-11-17 17:05:12 +0200

    webFragment add pick from gallery

--- app/src/main/java/com/vladtop/game_task/presentation/Web/WebFragment.kt
+++ app/src/main/java/com/vladtop/game_task/presentation/Web/WebFragment.kt
@@ -1,10 +1,16 @@
 package com.vladtop.game_task.presentation.Web
 
+import android.Manifest
 import android.annotation.SuppressLint
-import android.icu.text.SimpleDateFormat
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.webkit.ValueCallback
@@ -13,17 +19,25 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.vladtop.game_task.R
 import com.vladtop.game_task.data.WEB_LINK_KEY
 import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
 
+private const val PICK_IMAGE = 1
 
 class WebFragment : Fragment(R.layout.fragment_web) {
 
     private lateinit var webView: WebView
     private lateinit var webLink: String
+    private val TAG: String = "WebFragment"
+    private var mCM: String? = null
+    private var mUM: ValueCallback<Uri>? = null
+    private var mUMA: ValueCallback<Array<Uri>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,15 +81,83 @@ class WebFragment : Fragment(R.layout.fragment_web) {
 
     private fun getMyWebChromeClient(): WebChromeClient = object : WebChromeClient() {
 
+        @SuppressLint("QueryPermissionsNeeded")
         override fun onShowFileChooser(
             webView: WebView?,
             filePathCallback: ValueCallback<Array<Uri>>?,
             fileChooserParams: FileChooserParams?
         ): Boolean {
-            Toast.makeText(requireContext(), "onShowFileChooser", Toast.LENGTH_SHORT).show()
-            return super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
+
+            if (hasNoPermissions()) {
+                requestPermissions()
+            }
+            if (mUMA != null) {
+                mUMA!!.onReceiveValue(null)
+            }
+            mUMA = filePathCallback
+            var takePictureIntent: Intent? = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (takePictureIntent!!.resolveActivity(requireActivity().packageManager) != null) {
+                var photoFile: File? = null
+                try {
+                    photoFile = createImageFile()
+                    takePictureIntent.putExtra("PhotoPath", mCM)
+                } catch (ex: IOException) {
+                    Log.e(TAG, "Image file creation failed", ex)
+                }
+                if (photoFile != null) {
+                    mCM = "file:" + photoFile.absolutePath
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile))
+                } else {
+                    takePictureIntent = null
+                }
+            }
+            val contentSelectionIntent = Intent(Intent.ACTION_GET_CONTENT)
+            contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
+            contentSelectionIntent.type = "*/*"
+            val intentArray: Array<Intent?> =
+                takePictureIntent?.let { arrayOf(it) } ?: arrayOfNulls(0)
+
+            val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+            chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
+            chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
+            startActivityForResult(chooserIntent, PICK_IMAGE)
+
+            return true
         }
     }
+
+    private fun createImageFile(): File? {
+        @SuppressLint("SimpleDateFormat") val timeStamp =
+            SimpleDateFormat("yyyyMMdd_HHmmss").format(
+                Date()
+            )
+        val imageFileName = "img_" + timeStamp + "_"
+        val storageDir =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(imageFileName, ".jpg", storageDir)
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA),
+            PICK_IMAGE
+        )
+    }
+
+    private fun hasNoPermissions(): Boolean =
+        !(hasReadExternalPermission() && hasCameraPermission())
+
+    private fun hasReadExternalPermission(): Boolean = ActivityCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    ) != PackageManager.PERMISSION_GRANTED
+
+    private fun hasCameraPermission(): Boolean = ActivityCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.CAMERA
+    ) != PackageManager.PERMISSION_GRANTED
 
     private fun onBackPressed() {
         requireActivity().onBackPressedDispatcher.addCallback(
@@ -91,6 +173,43 @@ class WebFragment : Fragment(R.layout.fragment_web) {
                 }
             })
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if (Build.VERSION.SDK_INT >= 21) {
+            var results: Array<Uri>? = null
+            //Check if response is positive
+            if (resultCode == Activity.RESULT_OK) {
+                if (requestCode == PICK_IMAGE) {
+                    if (null == mUMA) {
+                        return
+                    }
+                    if (intent == null) {
+                        //Capture Photo if no image available
+                        if (mCM != null) {
+                            results = arrayOf(Uri.parse(mCM))
+                        }
+                    } else {
+                        val dataString = intent.dataString
+                        if (dataString != null) {
+                            results = arrayOf(Uri.parse(dataString))
+                        }
+                    }
+                }
+            }
+            mUMA?.onReceiveValue(results)
+            mUMA = null
+        }
+        if (requestCode == PICK_IMAGE) {
+            if (mUM == null) return
+            val result =
+                if (intent == null || resultCode != Activity.RESULT_OK) null else intent.data
+            mUM?.onReceiveValue(result)
+            mUM = null
+
+        }
+    }
+
 
     private fun showBackPressedMessage() {
         Toast.makeText(requireContext(), "Back Pressed", Toast.LENGTH_SHORT).show()
